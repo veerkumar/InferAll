b8bcf18146dd5ace2c07d07e994d1d053a5dcd10
veerkumar <pramodkumar5878@gmai.com>
2021-01-12 10:26:55 -0500

    dev

--- main_service_simulator.py
+++ main_service_simulator.py
@@ -44,9 +44,11 @@ class ScheduleLambdaEvent(Event):
                             current_time))
         return self.worker.execute_task(self.job_id, current_time)
 
+
+
 class Simulation(object):
 
-    def __init__(self, workload_file):
+    def __init__(self, workload_file, workload_type):
         self.workload_file = workload_file
 
         self.tasks = defaultdict()
@@ -55,6 +57,7 @@ class Simulation(object):
         self.VMs = defaultdict(lambda: np.ndarray(0))
         self.completed_VMs = defaultdict(list)
         self.lambdas = defaultdict()
+        self.workload_type = workload_type
         
         j = 0
         while j < INITIAL_WORKERS:
@@ -66,13 +69,33 @@ class Simulation(object):
     def run(self):
     	self.tasks_file = open(self.workload_file, 'r')
         line = self.tasks_file.readline()
-
+        start_time = 0
+        num_tasks = 0
+        task_type = 0
         # print line
-        if 
+        if self.workload_type == "tweeter_BATCH":
+        	start_time = float(line.split('\n')[0])
+        	num_tasks = 1
+        	task_type = 1
+        else: 
+	        start_time = float(line.split(',')[0])
+	        num_tasks = line.split(',')[3]
+	        task_type = line.split(',')[2]
 
-        start_time = float(line.split(',')[0])
-        num_tasks = line.split(',')[3]
-        task_type = line.split(',')[2]
+	    self.event_queue.put((start_time * 1000, TaskArrival(self,
+            start_time * 1000, num_tasks, task_type)))
+        last_time = 0
+        self.event_queue.put(((start_time*1000)+60000, PeriodicTimerEvent(self)))
+        self.event_queue.put(((start_time*1000)+60000, VM_Monitor_Event(self)))
+        while not self.event_queue.empty():
+            (current_time, event) = self.event_queue.get()
+            print current_time, event, self.event_queue.qsize()
+            #assert current_time >= last_time
+            last_time = current_time
+            new_events = event.run(current_time)
+            for new_event in new_events:
+                self.event_queue.put(new_event)
+        self.tasks_file.close()
 
 
 
--- task.py
+++ task.py
@@ -1,3 +1,60 @@
+
+class TaskArrival(Event):
+    """ Event to signify a job arriving at a scheduler. """
+    def __init__(
+            self,
+            simulation,
+            interarrival_delay,
+            num_tasks,
+            task_type,
+            ):
+        self.simulation = simulation
+        self.interarrival_delay = float(interarrival_delay)
+        self.num_tasks = int(num_tasks)
+        self.task_type = int(task_type)
+
+        # self.task_distribution= task_distribution
+
+    def run(self, current_time):
+        global last_task
+        task = Task(current_time, self.num_tasks, self.task_type)
+        logging.getLogger('sim').debug('Job %s arrived at %s'
+                % (task.id, current_time))
+
+        # Schedu1le job.
+
+        new_events = self.simulation.send_tasks(task, current_time)
+
+        # Add new Job Arrival event, for the next job to arrive after this one.
+
+        arrival_delay = random.expovariate(1.0
+                / self.interarrival_delay)
+
+        # new_events.append((current_time + arrival_delay, self))
+        #print("adding self task arriva",new_events)
+
+        logging.getLogger('sim').debug('Retuning %s events'
+                % len(new_events))
+        line = self.simulation.tasks_file.readline()
+        #print line
+        if line == '':
+            print('task empty')
+            last_task = 1
+            return new_events
+        start_time = float(line.split(',')[0])
+        num_tasks = line.split(',')[3]
+        task_type = line.split(',')[2]
+
+        #print "adding new task",int(start_time*1000), num_tasks, task_type
+        # new_task = Task(self, line, 1000, start_time, num_tasks, task_type)
+
+        new_events.append((start_time * 1000,
+            TaskArrival(self.simulation, start_time
+                * 1000, num_tasks, task_type)))
+        #print ("task arrival new events", new_events)
+        return new_events
+
+
 class Task(object):
 
     task_count = 0
